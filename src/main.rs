3ebc26405146bbd5e49f80f058d7810e93e4a061
Garrett-In-Development <critesg@spu.edu>
2023-04-06 01:16:59 -0700

    Getting Somewhere

--- src/main.rs
+++ src/main.rs
@@ -1,87 +1,32 @@
-struct Node {
+
+
+#[derive(Clone, PartialEq)]
+pub struct Node {
     value: String,
-    next: Option<Box<Node>>,
+    next: Option<Box<Node>>
 }
 
-struct MyList {
-    head: Option<Box<Node>>,
-    size: i32
-}
-
-impl MyList {
-    fn add(&mut self, i_key:i32, string1:String) -> bool {
-
-        if self.size == 0 {
-            self.head = Some(Box::new(Node{
-                value: string1,
-                next: None
-            }));
-            self.size += 1;
-            true
-        }
-        else {
-            if i_key > self.size {
-                false
-            }
-            else {
-                let mut index: i32 = 0;
-                let ref mut head: Option<Box<Node>> = self.head;
-                while index < i_key {
-                    match head {
-                        None => None,
-                        Some(i) => {
-                            println!("{}", i.value);
-                            match i.next {
-                                None => None,
-                                Some(j) => {
-                                    println!("{}", j.value);
-                                    head = Some(j);
-                                    Some(j)
-                                }
-                            }
-                        }
-                    };
-                    index += 1;
-                }
-
-                // match head {
-                //     None => println!("Null"),
-                //     Some(ref node) => println!("{}", node.value)
-                // };
-
-                true
-            }
-
-        }
-
+impl Node {
+    fn new(val: String) -> Node {
+       Node {
+        value: val,
+        next: None,
+       }
     }
-
-    // fn delete(&mut self, i_key:i32) {
-
-    // }
 }
-
-
-// fn commit() {
-
-// }
-
-// fn rollback() {
-
-// }
 
 fn main() {
-    let mut list = MyList {
-        head: Some(Box::new(Node{
-            value: String::from("Hello World"),
-            next: Some(Box::new(Node{
-                value: String::from("Second Node"),
-                next: None,
-            })),
-        })),
-        size: 2
-    };
+    let list = Some(Box::new(Node{value: String::from("Hello World"), next: Some(Box::new(Node::new(String::from("Second Node"))))}));
 
-    list.add(2, String::from("Third Node"));
-    list.add(3, String::from("Fourth Node"));
+    let mut head: Box<&Option<Box<Node>>> = Box::new(&list);
+
+    while *head != &None {
+        match *head {
+            None => (),
+            Some(ref i) => {
+                println!{"{}", i.value};
+                *head = &i.next;
+            }
+        }
+    }
 }
