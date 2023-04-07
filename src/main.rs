ba7aff1e57dd1269061546a5ee542790ca6280ec
Garrett-In-Development <critesg@spu.edu>
2023-04-07 16:23:50 -0700

    Read Concurrency Arc and Mutex, Implementing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
+use std::sync::{Arc, Mutex};
 
-
-#[derive(Clone, PartialEq)]
+#[derive(Clone)]
 pub struct Node {
     value: String,
-    next: Option<Box<Node>>
+    next: Option<Arc<Mutex<Node>>>
 }
 
 impl Node {
@@ -15,18 +15,55 @@ impl Node {
     }
 }
 
-fn main() {
-    let list = Some(Box::new(Node{value: String::from("Hello World"), next: Some(Box::new(Node::new(String::from("Second Node"))))}));
+pub struct MyList {
+    head: Option<Arc<Mutex<Node>>>,
+    size: usize,
+}
 
-    let mut head: Box<&Option<Box<Node>>> = Box::new(&list);
+impl MyList {
 
-    while *head != &None {
-        match *head {
-            None => (),
-            Some(ref i) => {
-                println!{"{}", i.value};
-                *head = &i.next;
-            }
+    fn new() -> MyList {
+        MyList {
+            head: None,
+            size: 0
         }
     }
+
+    fn add(&mut self, i_key: usize, string1: &str) -> Result<(), String> {
+        let new_node = Arc::new(Mutex::new(Node::new(String::from(string1))));
+
+
+    }
+
+    fn delete(&mut self, i_key: usize) -> Result<(), String> {
+
+    }
+
+    fn commit() {
+
+    }
+
+    fn rollback() {
+
+    }
+
+}
+
+fn main() {
+
+    // let mut head: &Option<Box<Node>> = &list;
+
+    // Traversal
+    // while *head != None {
+    //     match *head {
+    //         None => (),
+    //         Some(ref i) => {
+    //             println!{"{}", i.value};
+    //             head = &i.next;
+    //         }
+    //     }
+    // }
+
+
+
 }
